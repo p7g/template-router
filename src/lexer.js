b55ba3027bf89597fd5af5d76107c197720ea578
Patrick Gingras <775.pg.12@gmail.com>
2019-02-15 12:09:24 -0500

    moved logic to read identifier into own function

--- src/lexer.js
+++ src/lexer.js
@@ -8,19 +8,23 @@ module.exports = function lex(input) {
   const valid = () => current() !== undefined;
   const advance = (amount = 1) => { position += amount; };
   const retreat = (amount = 1) => { position -= amount; };
+  const captureIdentifier = () => {
+    let name = '';
+    while (isLetter(current())) {
+      name += current();
+      advance();
+    }
+    retreat();
+    return { type: 'identifier', name };
+  };
 
-  for (; valid(); position += 1) {
+  while (valid()) {
     if (isLetter(current())) {
-      let name = '';
-      while (isLetter(current())) {
-        name += current();
-        advance();
-      }
-      retreat();
-      tokens.push({ type: 'identifier', name });
+      tokens.push(captureIdentifier());
     } else if (current() === ',') {
       tokens.push({ type: 'comma' });
     }
+    advance();
   }
 
   return tokens;
